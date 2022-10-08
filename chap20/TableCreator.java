050a0117dad885f69f68e2f80cdecd3d55893a02
coderforcoder <18356996925@163.com>
2022-10-09 00:35:08 +0800

    start learn annotation

---
+++ chap20/TableCreator.java
@@ -0,0 +1,10 @@
+/**
+ * @author 糖小样的皮卡丘
+ */
+public class TableCreator {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println();
+        }
+    }
+}
